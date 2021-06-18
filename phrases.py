9ca154c00cd4e477e6d57129c323ba4aa0bc8f6f
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2021-06-19 01:42:03 +0900

    [fix] description of command

--- phrases.py
+++ phrases.py
@@ -4,4 +4,4 @@ activity = 't:info'
 
 aboutText = '毎日22時に気象庁発表の翌日の天気の情報を書き込むbotです。\n詳しい仕様はGitHubリポジトリ( https://github.com/Natsu-dev/otenki )に掲載していますので、必ずご一読ください。'
 
-commandList = '`t:info` botの説明が表示されます。\n`t:forecast` 全国版の明日の天気が表示されます。'
+commandList = '`t:info` botの説明が表示されます。\n`t:forecast` 全国版の今日の天気が表示されます。'
